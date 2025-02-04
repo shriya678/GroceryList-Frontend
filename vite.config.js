e4119e2fcae8ff17ac6f298faa3dd93d0203a36d
shriya678 <guptashriya035@gmail.com>
2025-02-04 19:48:22 +0530

    added backend vercel url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://grocery-list-backend-delta.vercel.app",
         changeOrigin: true,
       },
     }
